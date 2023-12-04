a3106e293ef402f9cead448686f9c68fb5a2fad8
Santiago <santiago.garciah17@gmail.com>
2023-12-04 16:36:30 -0500

    Mappers actualizados

--- target/generated-sources/annotations/com/prevalentware/prueba_tecnica/application/mapper/response/ISessionResponseMapperImpl.java
+++ target/generated-sources/annotations/com/prevalentware/prueba_tecnica/application/mapper/response/ISessionResponseMapperImpl.java
@@ -15,7 +15,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2023-12-04T16:11:22-0500",
+    date = "2023-12-04T16:35:49-0500",
     comments = "version: 1.5.5.Final, compiler: javac, environment: Java 17.0.9 (Amazon.com Inc.)"
 )
 @Component
